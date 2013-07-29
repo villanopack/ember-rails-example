5e8d6c72a3d119d0d4e7c85b099940e7ce259689
villanopack <villa@dvillanueva.com>
2013-07-29 10:59:49 +0200

    About y deshabilitado el has_many de ember-data porque da problemas a la hora de crear recursos con un error each.nil , supongo que pretende recorrer el array de la asociación y como es nil , casca. Habria que crear el recurso siempre con su asociacion

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -2,8 +2,10 @@ App.Adapter = DS.RESTAdapter.extend({
   bulkCommit: false
 });
 
+
 App.Adapter.map('App.Film', {
-  actors: {embedded: 'always'}
+//  actors: {embedded: 'always'}
+  actors: {embedded: 'inline'}
 });
 
 App.Store = DS.Store.extend({
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :necesita_ember
+
+  def about
+
+  end
+
+  private
+
+  def necesita_ember
+    if params[:action] == "about"
+      logger.info "entra en about"
+      "application_sin"
+    else
+      "application"
+    end
+  end
+
 end
